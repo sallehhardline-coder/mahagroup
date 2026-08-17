1ad313bc733c150acd72997d4d6317d46d17985c
sallehhardline-coder <sallehhardline@gmail.com>
2026-08-18 05:58:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { Telegraf } = require('telegraf');
 const express = require('express');
 
-// Express server biar Render gak ngiranya server mati
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Bot Aktif!'));
-app.listen(PORT, () => console.log(`Server jalan di port ${PORT}`));
+const port = process.env.PORT || 3000;
 
-// Inisialisasi Bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Halo! Bot kamu udah aktif 24/7 nih!'));
-bot.help((ctx) => ctx.reply('Ada yang bisa dibantu?'));
-bot.on('text', (ctx) => ctx.reply(`Kamu ngetik: ${ctx.message.text}`));
+app.get('/', (req, res) => {
+  res.send('Bot Telegram Aktif!');
+});
 
 bot.launch();
+
+app.listen(port, () => {
+  console.log(`Server berjalan di port ${port}`);
+});
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
